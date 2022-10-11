7ee9a8bf02709c33cdbcbdf72dd237693a4cef5f
lelee23 <113265900+lelee23@users.noreply.github.com>
2022-10-11 16:47:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+
+$('.box2 img').click(function(){
+    $('#foto-grande').fadeOut(0);
+    caminho = $(this).attr('data-url');
+    $('#foto-grande').attr('src',caminho);
+    $('#foto-grande').fadeIn(900);
+})
+
+function readImage() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("preview").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+document.getElementById("img-input").addEventListener("change", readImage, false);
+
+function Avaliar(estrela){
+    var url = window.location;
+    url = url.toString()
+    url = url.split("index.html");
+    url = url[0];
+
+    var s1 = document.getElementById("s1").src;
+    var s2 = document.getElementById("s2").src;
+    var s3 = document.getElementById("s3").src;
+    var s4 = document.getElementById("s4").src;
+    var s5 = document.getElementById("s5").src;
+
+    if(estrela == 5){
+        if(s5 == url + "img/star0.png"){
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star1.png";
+            document.getElementById("s4").src = "img/star1.png";
+            document.getElementById("s5").src = "img/star1.png";
+        }else{
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star1.png";
+            document.getElementById("s4").src = "img/star1.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }
+    }
+    if(estrela == 4){
+        if(s4 == url + "img/star0.png"){
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star1.png";
+            document.getElementById("s4").src = "img/star1.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }else{
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star1.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }
+    }
+    if(estrela == 3){
+        if(s3 == url + "img/star0.png"){
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star1.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }else{
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star0.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }
+    }
+    if(estrela == 2){
+        if(s2 == url + "img/star0.png"){
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star1.png";
+            document.getElementById("s3").src = "img/star0.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }else{
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star0.png";
+            document.getElementById("s3").src = "img/star0.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }
+    }
+    if(estrela == 1){
+        if(s1 == url + "img/star0.png"){
+            document.getElementById("s1").src = "img/star1.png";
+            document.getElementById("s2").src = "img/star0.png";
+            document.getElementById("s3").src = "img/star0.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }else{
+            document.getElementById("s1").src = "img/star0.png";
+            document.getElementById("s2").src = "img/star0.png";
+            document.getElementById("s3").src = "img/star0.png";
+            document.getElementById("s4").src = "img/star0.png";
+            document.getElementById("s5").src = "img/star0.png";
+        }
+    }
+}
